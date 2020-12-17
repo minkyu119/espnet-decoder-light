80dbc060eed4e0a36ab107836072d5d1eaf493ad
minkyu119 <minkyu119@paran.com>
2020-12-17 16:48:03 +0900

    Update predict.py

--- predict/predict.py
+++ predict/predict.py
@@ -9,6 +9,8 @@ def predict(filename=None,AR=True):
     parser=get_parser()
     args=parser.parse()
     args.model='./model.pt'
+    if AR is False: #Trandformer 인코더만 사용하여 결과 출력, 인식률 하락, 속도 20배
+        args.fast_decode=True
     a = read(args.filename)
     data = numpy.array(a[1],dtype=numpy.int16)
     asr=SpeechRecognizer(args)
